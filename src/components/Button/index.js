a2f8ed5e2e760bb427afda9327d0dd098d4d641c
Vin <phamvanvinh8a2@gmail.com>
2023-02-06 01:52:27 +0700

    add Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,68 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    onClick,
+    primary = false,
+    rounded = false,
+    text = false,
+    outline = false,
+    disabled = false,
+    small = false,
+    large = false,
+    children,
+    className,
+    LeftIcon,
+    RightIcon,
+    ...otherProps
+}) {
+    let Component = 'button';
+
+    const props = {
+        onClick,
+        ...otherProps,
+    };
+
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            let isEventListener = key.startsWith('on') && typeof props[key] === 'function';
+            if (isEventListener) {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Component = Link;
+    } else if (href) {
+        props.href = href;
+        Component = 'a';
+    }
+
+    const classes = cx('wrapper', {
+        text,
+        primary,
+        outline,
+        rounded,
+        disabled,
+        small,
+        large,
+        [className]: className,
+    });
+
+    return (
+        <Component className={classes} {...props}>
+            {LeftIcon && <LeftIcon className={cx('left-icon', 'icon')} />}
+            <span className={cx('title')}>{children}</span>
+            {RightIcon && <RightIcon className={cx('right-icon', 'icon')} />}
+        </Component>
+    );
+}
+
+export default Button;
