ed86479dc67911f2032c0002794eb3b3d8dccb31
Vin <phamvanvinh8a2@gmail.com>
2023-02-09 17:31:09 +0700

    modify AccountItem

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,25 +1,22 @@
 import classNames from 'classnames/bind';
-import styles from './Account.module.scss';
+import styles from './AccountItem.module.scss';
 import { CheckCircleIcon } from '~/components/Icons';
 import Image from '~/components/Image';
+import { Link } from 'react-router-dom';
 const cx = classNames.bind(styles);
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src="https://scontent.fdad3-1.fna.fbcdn.net/v/t1.6435-9/54798713_839835193036669_8703457475350757376_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=174925&_nc_ohc=1s1HTYa22DgAX-JfXCi&_nc_ht=scontent.fdad3-1.fna&oh=00_AfD-2HD6RtkW_1YRCoKMrWXKCYKzH019zjXjEo1hLKSlAw&oe=640B09DE"
-                alt="Hoa"
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.nickname} />
             <div className={cx('info')}>
                 <h4 className={cx('username')}>
-                    <span>nguyenvana</span>
-                    <CheckCircleIcon />
+                    <span>{data.nickname}</span>
+                    {data.tick && <CheckCircleIcon />}
                 </h4>
-                <p className={cx('fullname')}>Nguyễn Văn A</p>
+                <p className={cx('fullname')}>{data.full_name}</p>
             </div>
-        </div>
+        </Link>
     );
 }
 
