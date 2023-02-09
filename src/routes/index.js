6f462e9b0cc3615c3a57324ec1698857ffa8ec94
Vin <phamvanvinh8a2@gmail.com>
2023-02-09 17:26:14 +0700

    add Profile page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { HeaderOnly } from '~/components/Layouts';
 
 const { default: Home } = require('~/pages/Home');
+const { default: Profile } = require('~/pages/Profile');
 const { default: Following } = require('~/pages/Following');
 const { default: Upload } = require('~/pages/Upload');
 
 const publicRoutes = [
     { path: '/', component: Home },
+    { path: '/@:nickname', component: Profile },
     { path: '/following', component: Following },
     { path: '/upload', component: Upload, layout: HeaderOnly },
 ];
