baf13971270689e64b17997c8f87dcb314064eff
Vin <phamvanvinh8a2@gmail.com>
2023-02-09 17:30:15 +0700

    add LoaderIcon

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -132,7 +132,7 @@ export function SearchIcon({ className, size = '1em' }) {
             width={size}
             height={size}
             viewBox="0 0 48 48"
-            fill="rgba(22, 24, 35, .34)"
+            fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"
         >
             <path
@@ -332,7 +332,7 @@ export function XCircleIcon({ className, size = '1em' }) {
             width={size}
             height={size}
             viewBox="0 0 48 48"
-            fill="rgba(22, 24, 35, .34)"
+            fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"
         >
             <path
@@ -373,7 +373,7 @@ export function CheckCircleIcon({ className, size = '1em' }) {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
         >
-            <circle cx="24" cy="24" r="24" fill="#20D5EC"></circle>
+            <circle cx="24" cy="24" r="24" fill="currentColor"></circle>
             <path
                 fillRule="evenodd"
                 clipRule="evenodd"
@@ -398,6 +398,25 @@ export function ChevronLeftIcon({ className, size = '1em' }) {
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M4.58579 22.5858L20.8787 6.29289C21.2692 5.90237 21.9024 5.90237 22.2929 6.29289L23.7071 7.70711C24.0976 8.09763 24.0976 8.7308 23.7071 9.12132L8.82843 24L23.7071 38.8787C24.0976 39.2692 24.0976 39.9024 23.7071 40.2929L22.2929 41.7071C21.9024 42.0976 21.2692 42.0976 20.8787 41.7071L4.58579 25.4142C3.80474 24.6332 3.80474 23.3668 4.58579 22.5858Z"
+            ></path>
+        </svg>
+    );
+}
+
+export function LoaderIcon({ className, size = '1em' }) {
+    return (
+        <svg
+            className={className}
+            width={size}
+            height={size}
+            viewBox="0 0 48 48"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M24 12.5C17.6487 12.5 12.5 17.6487 12.5 24C12.5 30.3513 17.6487 35.5 24 35.5C26.8172 35.5 29.3919 34.4902 31.3919 32.8101C32.4491 31.9219 34.026 32.059 34.9142 33.1161C35.8023 34.1733 35.6653 35.7503 34.6081 36.6384C31.741 39.0471 28.0369 40.5 24 40.5C14.8873 40.5 7.5 33.1127 7.5 24C7.5 14.8873 14.8873 7.5 24 7.5C33.1127 7.5 40.5 14.8873 40.5 24C40.5 25.3807 39.3807 26.5 38 26.5C36.6193 26.5 35.5 25.3807 35.5 24C35.5 17.6487 30.3513 12.5 24 12.5Z"
             ></path>
         </svg>
     );
