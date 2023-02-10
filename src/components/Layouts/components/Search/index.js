51352ae1ca0d155c6d54d27fa76f2363d97c475f
Vin <phamvanvinh8a2@gmail.com>
2023-02-11 02:30:39 +0700

    use <div> to wrap interactive tippy

--- src/components/Layouts/components/Search/index.js
+++ src/components/Layouts/components/Search/index.js
@@ -69,46 +69,53 @@ function Search() {
     }, [debounce]);
 
     return (
-        <TippyHeadless
-            interactive
-            visible={isResultShown && searchResult.length > 0}
-            render={(attrs) => (
-                <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper>
-                        <p className={cx('accounts-title')}>Accounts</p>
-                        {searchResult.map((result) => {
-                            return <AccountItem key={result.id} data={result} />;
-                        })}
-                    </PopperWrapper>
-                </div>
-            )}
-            onClickOutside={handleHidingResult}
-        >
-            <div className={cx('search')}>
-                <input
-                    ref={inputRef}
-                    value={inputValue}
-                    spellCheck="false"
-                    placeholder="Search accounts and videos"
-                    onChange={handleChanging}
-                    onFocus={() => setIsResultShown(true)}
-                />
+        // Interactive tippy element may not be accessible via keyboard navigation
+        // because it is not directly after the reference element in the DOM source order
 
-                {!isLoading && (
-                    <button className={cx('search-clear')} onClick={handleClearingSearch}>
-                        <XCircleIcon />
+        // Using a wrapper <div> or <span> tag around the reference element solves this
+        // by creating a new parentNode context.
+        <div>
+            <TippyHeadless
+                interactive
+                visible={isResultShown && searchResult.length > 0}
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <p className={cx('accounts-title')}>Accounts</p>
+                            {searchResult.map((result) => {
+                                return <AccountItem key={result.id} data={result} />;
+                            })}
+                        </PopperWrapper>
+                    </div>
+                )}
+                onClickOutside={handleHidingResult}
+            >
+                <div className={cx('search')}>
+                    <input
+                        ref={inputRef}
+                        value={inputValue}
+                        spellCheck="false"
+                        placeholder="Search accounts and videos"
+                        onChange={handleChanging}
+                        onFocus={() => setIsResultShown(true)}
+                    />
+
+                    {!isLoading && (
+                        <button className={cx('search-clear')} onClick={handleClearingSearch}>
+                            <XCircleIcon />
+                        </button>
+                    )}
+                    {isLoading && (
+                        <p className={cx('search-loader', 'hide')}>
+                            <LoaderIcon />
+                        </p>
+                    )}
+                    <button className={cx('search-btn')} onMouseDown={handleSubmitting}>
+                        <SearchIcon />
                     </button>
-                )}
-                {isLoading && (
-                    <p className={cx('search-loader', 'hide')}>
-                        <LoaderIcon />
-                    </p>
-                )}
-                <button className={cx('search-btn')} onMouseDown={handleSubmitting}>
-                    <SearchIcon />
-                </button>
-            </div>
-        </TippyHeadless>
+                </div>
+            </TippyHeadless>
+        </div>
     );
 }
 
