300800340f3c448ac110c6064efecb14f2f43729
Vin <phamvanvinh8a2@gmail.com>
2023-02-12 03:29:45 +0700

    Sidebar

---
+++ src/layouts/DefaultLayout/Sidebar/Menu/MenuItem.js
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
+import styles from './Menu.module.scss';
+
+const cx = classNames.bind(styles);
+
+function MenuItem({ title, to, icon, activeIcon = icon }) {
+    return (
+        <NavLink to={to} className={(nav) => cx('menu-item', { active: nav.isActive })} end>
+            <span className={cx('icon')}>{icon}</span>
+            <span className={cx('icon-active')}>{activeIcon}</span>
+            <span className={cx('title')}>{title}</span>
+        </NavLink>
+    );
+}
+
+MenuItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    to: PropTypes.string.isRequired,
+    icon: PropTypes.node.isRequired,
+    activeIcon: PropTypes.node,
+};
+
+export default MenuItem;
--- src/layouts/DefaultLayout/Sidebar/Sidebar.js
+++ src/layouts/DefaultLayout/Sidebar/Sidebar.js
@@ -1,10 +1,39 @@
 import classNames from 'classnames/bind';
+import config from '~/config';
+import Menu, { MenuItem } from './Menu';
 import styles from './Sidebar.module.scss';
+
+import {
+    HomeIcon,
+    HomeOutlineIcon,
+    LiveIcon,
+    LiveOutlineIcon,
+    UserGroupIcon,
+    UserGroupOutlineIcon,
+} from '~/components/icons';
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
-    return <aside className={cx('wrapper')}>Sidebar</aside>;
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem
+                    title="For You"
+                    to={config.routes.home}
+                    icon={<HomeOutlineIcon />}
+                    activeIcon={<HomeIcon />}
+                />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupOutlineIcon />}
+                    activeIcon={<UserGroupIcon />}
+                />
+                <MenuItem title="Live" to={config.routes.live} icon={<LiveOutlineIcon />} activeIcon={<LiveIcon />} />
+            </Menu>
+        </aside>
+    );
 }
 
 export default Sidebar;
